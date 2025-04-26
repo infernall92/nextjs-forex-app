b3666a70386775b36032d201871fd1a0a888631a
Hristo <hhristo.hristovv@gmail.com>
2025-04-26 11:56:33 +0300

    fix routing and styling issues

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   output: "export",
   images: { unoptimized: true },
   basePath: "/nextjs-forex-app",
-  assetPrefix: "/nextjs-forex-app/",
+  assetPrefix: "/nextjs-forex-app",
 };
 
 export default nextConfig;
