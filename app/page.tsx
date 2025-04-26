94b083abbb3b89bdb3d56afebe3746131f1ee7c0
Hristo <hhristo.hristovv@gmail.com>
2025-04-26 11:45:39 +0300

    fix routing

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
       <h1 className={classes.text}>This is the home page</h1>
       <p className={classes.text}>nothing special</p>
       <h3 className={classes.text}>
-        But you can click <Link href="/nextjs-forex-app/rates">HERE</Link> to
-        check the currency exchange rates
+        But you can click <Link href="./rates">HERE</Link> to check the currency
+        exchange rates
       </h3>
     </main>
   );
